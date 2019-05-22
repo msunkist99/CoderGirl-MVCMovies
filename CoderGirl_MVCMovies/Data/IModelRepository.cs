cda6cef4e916a41dc8e17dc3432c1b49d3df8812
msunkist99 <msunkist99@gmail.com>
2019-05-22 17:05:23 -0500

    20190522

---
+++ CoderGirl_MVCMovies/Data/IModelRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoderGirl_MVCMovies.Models;
+
+namespace CoderGirl_MVCMovies.Data
+{
+    interface IModelRepository
+    {
+        void Delete(int id);
+
+        IModel GetById(int id);
+
+        List<IModel> GetModels();
+
+        int Save(IModel model);
+
+        void Update(IModel model);
+
+    }
+}
